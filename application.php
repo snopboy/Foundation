d555f66779d75a07ad71b37422a6e6585fa97467
Or (Snopboy) <shukrun.or@gmail.com>
2016-06-02 00:24:34 +0300

    Minor cleanups in application.php

--- application.php
+++ application.php
@@ -3,19 +3,6 @@
 use Foundation\Framework;
 use Illuminate\Container\Container;
 
-use Foundation\Controller\BaseController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\Generator\UrlGenerator;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpKernel\HttpKernel;
-use Symfony\Component\HttpKernel\EventListener\RouterListener;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-
 
 Container::setInstance(new Container());
 $container = Container::getInstance();
@@ -38,36 +25,4 @@ if (! function_exists('app')) {
 	}
 }
 
-$foundation = Framework::singleton()->registerContainer($container)->Boot();
-/*
-$routes = new RouteCollection();                        // Router
-require(CONF_PATH.'routes.php');                        // Router
-$container->instance('http.route_collection', $routes); // Router
-
-$container->instance('http.request', Request::createFromGlobals());  // request
-$container->instance('http.request.content', new RequestContext());  // request
-$matcher = new UrlMatcher($routes, app('http.request.content'));     // request
-$generator = new UrlGenerator($routes, app('http.request.content')); // request
-
-$container->instance('http.matcher', $matcher);
-$container->instance('http.generator', $generator);
-
-$container->instance('events.dispatcher', new EventDispatcher());
-app('events.dispatcher')->addSubscriber(new RouterListener(app('http.matcher'), new RequestStack()));
-
-$container->instance('http.controller.resolver', new ControllerResolver());
-$container->instance('http.kernel', new HttpKernel(app('events.dispatcher'), app('http.controller.resolver')));
-
-
-$container->instance('app.base_controller', new BaseController());
-$response = app('app.base_controller')->forge(app('http.request'), app('http.controller.resolver'), $matcher, $container);
-
-
-if (!$response instanceof Response) {
-	$response = new Response($response);
-	$response->setStatusCode(app('app.base_controller')->header);
-}
-$container->instance('http.response', $response);
-app('http.response')->send();
-
-app('http.kernel')->terminate(app('http.request'), app('http.response'));*/
+Framework::singleton()->registerContainer($container)->Boot();
